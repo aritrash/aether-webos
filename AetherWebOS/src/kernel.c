16f5e92b45b3dc1e7bae5e87caaab092f698b0fb
Aritrash Sarkar <aritrashsarkar@gmail.com>
2026-02-08 11:04:42 +0530

    Looping boot initialized

---
+++ AetherWebOS/src/kernel.c
@@ -0,0 +1,9 @@
+void kernel_main(){
+    //For now, this is a placeholder.
+    // Add UART driver code next to print AetherWebOS logo and version
+    // info on remote screen via Serial.
+
+    while(1){
+        // CPU heartbeat loop
+    }
+}
